b7699c235223e4c6eea0c6388bbb468aa76e3eca
Dikshas09 <dikshasinghal858@gmail.com>
2025-11-14 22:00:14 +0530

    m

---
+++ day-1.cpp/operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=-3;
+    int b=4;
+    cout<<a%b<<endl;
+    int c=3;
+    int d=-4;
+    cout<<c%d<<endl;
+    int e;
+    cin>>e;
+    cout<<e;
+}
