8a8a1b74190a204adb819f75f65586c920dc9af9
Dikshas09 <dikshasinghal858@gmail.com>
2025-11-14 22:22:53 +0530

    m

--- day-1.cpp/operator.cpp
+++ day-1.cpp/operator.cpp
@@ -8,7 +8,13 @@ int main(){
     int c=3;
     int d=-4;
     cout<<c%d<<endl;
+    int z=-3;
+    int u=-4;
+    cout<<z%u<<endl;
     int e;
     cin>>e;
-    cout<<e;
+    cout<<e<<endl;
+    char ch= 'f' ;
+    int x= (int)ch; 
+    cout<<x<<endl;
 }
