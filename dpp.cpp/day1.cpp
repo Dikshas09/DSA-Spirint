135d545defd960a3528e7d9fc389d33aa11a6fcf
Dikshas09 <dikshasinghal858@gmail.com>
2026-02-17 15:46:46 +0530

    day-1.cpp/reverse.cpp/string.cpp

---
+++ dpp.cpp/day1.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int e,f;
+    cin>>e;
+    cin>>f;
+    cout<<"sum of = "<<e+f<<endl;
+    cout<<"diff. of = "<<e-f<<endl;
+    cout<<"multiply of = "<<e*f<<endl;
+    cout<<"division of = "<<e/f<<endl;
+//for finding value of x..............................    
+    int x=3;
+    cout<<x<<endl;
+    ++x;
+    cout<<x<<endl;
+    x++;
+    cout<<x<<endl;
+// for performing division.......................
+   int a,b;
+   cin>>a;
+   cin>>b;
+   cout<<a%b<<endl;
+   cout<<b%a<<endl;
+// ascii value...............................
+    char ch= 'f' ;
+    int x= (int)ch; 
+    cout<<x<<endl; 
+// input integer and get character...........................
+   int c;
+   cin>>c;
+   char cha;
+   cha =char(c);
+   cout<<cha<<endl; 
+// for getting remainder.......................................   
+   int a,b;
+   cin>>a;
+   cin>>b;
+   cout<<a%b<<endl;   
+}
